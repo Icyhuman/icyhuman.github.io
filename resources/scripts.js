1ce7cdc6e3aa41b07d58f88681bceed270496d75
Icyhuman <jorgen.storum@gmail.com>
2026-03-09 17:01:37 +0100

    adjustment to color box

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,9 +1,9 @@
 function recolor(event){ //get mouse pos, make color
   let x = event.clientX;
   let y = event.clientY;
-  let r = x;
-  let g = 255 - x;
-  let b = y - 485;
+  let r = (Math.cos(0.05*x)+1)*127;
+  let g = (Math.sin(0.03*x)+1)*63 + (Math.sin(0.03*y)+1)*64;
+  let b = (Math.cos(0.05*y)+1)*127;
   let rgebe="rgb("+r+","+g+","+b+")";
   document.getElementById("dabox").style.background = rgebe;
 }
