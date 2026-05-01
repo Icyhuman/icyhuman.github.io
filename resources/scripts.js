428c64b6f30d471d0fa3ce4af15e2fd7600546ce
Icyhuman <jorgen.storum@gmail.com>
2026-05-01 19:42:41 +0200

    Toned down the stupid color box

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,9 +1,9 @@
 function recolor(event){ //get mouse pos, make color
   let x = event.clientX;
   let y = event.clientY;
-  let r = (Math.cos(0.05*x)+1)*127;
-  let g = (Math.sin(0.03*x)+1)*63 + (Math.sin(0.03*y)+1)*64;
-  let b = (Math.cos(0.05*y)+1)*127;
+  let r = (Math.cos(0.025*x)+1)*127;
+  let g = (Math.sin(0.015*x)+1)*63 + (Math.sin(0.015*y)+1)*64;
+  let b = (Math.cos(0.025*y)+1)*127;
   let rgebe="rgb("+r+","+g+","+b+")";
   document.getElementById("dabox").style.background = rgebe;
 }
